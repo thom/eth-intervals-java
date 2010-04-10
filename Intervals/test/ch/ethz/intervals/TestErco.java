7e51b4b76a2cfeff6c56f71f9eb4600bbbb443f0
Thomas Weibel <thom@beeblebrox.net>
2010-04-10 14:46:25 +0200

    Move erco benchmarks (elevator, tsp and sor) to test

--- Intervals/test/ch/ethz/intervals/TestErco.java
+++ Intervals/test/ch/ethz/intervals/TestErco.java
@@ -5,12 +5,12 @@ import java.io.IOException;
 import org.junit.Assert;
 import org.junit.Test;
 
-import erco.intervals.tsp.Tsp;
+import erco.tsp.intervals.Tsp;
 
 public class TestErco {
 	@Test public void TspMap16z() throws IOException {		
 		Tsp tsp = new Tsp();
-		int[] tour = tsp.solve("erco_test/benchmarks/tsp/tspfiles/map16z");
+		int[] tour = tsp.solve("test/erco/tsp/tspfiles/map16z");
 		int[] expected = new int[] {
 			0, 12, 10, 15, 7, 6, 4, 5, 1, 13, 14, 3, 2, 8, 11, 9, 0
 		};
