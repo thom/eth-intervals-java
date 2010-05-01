9bb5c72373b2183d1c54ec21ed5775a1902437c4
Thomas Weibel <thom@beeblebrox.net>
2010-05-01 20:20:02 +0200

    Add statistics for puts, takes (attempts, successes, failures), steals (attempts, successes, failures), grows

--- Intervals/src/ch/ethz/intervals/WorkerStatistics.java
+++ Intervals/src/ch/ethz/intervals/WorkerStatistics.java
@@ -1,5 +1,7 @@
 package ch.ethz.intervals;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 import ch.ethz.intervals.ThreadPool.Worker;
 
 public class WorkerStatistics {
@@ -7,8 +9,14 @@ public class WorkerStatistics {
 
 	private final Worker owner;
 
-	// TODO: Count grows, steals (attempt, successful, unsuccessful), puts,
-	// takes (attempt, successful, unsuccessful)
+	private AtomicInteger puts = new AtomicInteger(0);
+	private AtomicInteger takeAttempts = new AtomicInteger(0);
+	private AtomicInteger takeSuccesses = new AtomicInteger(0);
+	private AtomicInteger takeFailures = new AtomicInteger(0);
+	private AtomicInteger stealAttempts = new AtomicInteger(0);
+	private AtomicInteger stealSuccesses = new AtomicInteger(0);
+	private AtomicInteger stealFailures = new AtomicInteger(0);
+	private AtomicInteger grows = new AtomicInteger(0);
 
 	public WorkerStatistics(Worker owner) {
 		// all statistics events should be protected by if(ENABLED)
@@ -17,15 +25,63 @@ public class WorkerStatistics {
 		this.owner = owner;
 	}
 
+	public void doPut() {
+		assert ENABLED;
+		puts.incrementAndGet();
+	}
+
+	public void doTakeAttempt() {
+		assert ENABLED;
+		takeAttempts.incrementAndGet();
+	}
+
+	public void doTakeSuccess() {
+		assert ENABLED;
+		takeSuccesses.incrementAndGet();
+	}
+
+	public void doTakeFailure() {
+		assert ENABLED;
+		takeFailures.incrementAndGet();
+	}
+
+	public void doStealAttempt() {
+		assert ENABLED;
+		stealAttempts.incrementAndGet();
+	}
+
+	public void doStealSuccess() {
+		assert ENABLED;
+		stealSuccesses.incrementAndGet();
+	}
+
+	public void doStealFailure() {
+		assert ENABLED;
+		stealFailures.incrementAndGet();
+	}
+
+	public void doGrow() {
+		assert ENABLED;
+		grows.incrementAndGet();
+	}
+
 	public String toString() {
 		assert ENABLED;
 
 		String result = "\nStatistics for " + owner + "\n\n";
-		result += "TODO";
-		result += "\n";
+		result += "Puts\n";
+		result += puts + "\n\n";
+		result += "Take Attempts\tTake Successes\tTake Failures\n";
+		result += takeAttempts + "\t\t" + takeSuccesses + "\t\t" + takeFailures
+				+ "\n\n";
+		result += "Steal Attempts\tSteal Successes\tSteal Failures\n";
+		result += stealAttempts + "\t\t" + stealSuccesses + "\t\t"
+				+ stealFailures + "\n\n";
+		result += "Deque Grows\n";
+		result += grows + "\n";
 		return result;
 	}
-	
+
 	public void print() {
 		assert ENABLED;
 		System.err.println(this);
