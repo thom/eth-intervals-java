9fed1861750ff8c65d75cb3e0a5d992acf8c8734
Thomas Weibel <thom@beeblebrox.net>
2010-05-04 11:14:04 +0200

    Update config class to support duplicating queues

--- Intervals/src/ch/ethz/intervals/Config.java
+++ Intervals/src/ch/ethz/intervals/Config.java
@@ -3,10 +3,26 @@ package ch.ethz.intervals;
 import ch.ethz.intervals.ThreadPool.Worker;
 
 public class Config {
+	/**
+	 * Should we collect and print statistics
+	 */
 	public static final boolean STATISTICS = false;
 
-	// TODO: DUPLICATING_QUEUE
+	/**
+	 * Are we using a duplicating queue?
+	 *
+	 * Use state (init, running, done) to make sure tasks are executed only
+	 * once.
+	 */
+	public static final boolean DUPLICATING_QUEUE = false;
 
+	/**
+	 * Creation method to simplify switching of work-stealing queue
+	 * implementations
+	 *
+	 * @param owner
+	 * @return WorkStealingDeque
+	 */
 	public final static WorkStealingQueue createQueue(Worker owner) {
 		return new LazyDeque(owner);
 	}
