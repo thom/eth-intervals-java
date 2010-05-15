54f597813f945bf5b8be740ae34fc4587aef9c4e
Thomas Weibel <thom@beeblebrox.net>
2010-05-15 17:59:43 +0200

    Skeleton for work-stealing lazy deque

---
+++ Intervals/src/ch/ethz/intervals/WorkStealingLazyDeque.java
@@ -0,0 +1,25 @@
+package ch.ethz.intervals;
+
+import ch.ethz.intervals.ThreadPool.Worker;
+
+public class WorkStealingLazyDeque implements WorkStealingQueue {
+
+	@Override
+	public void put(WorkItem task) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public WorkItem steal(Worker thiefWorker) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public WorkItem take() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
