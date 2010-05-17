c37ea9a506d702abcb3b23ebacc42039259d4aea
Thomas Weibel <thom@beeblebrox.net>
2010-05-17 18:08:50 +0200

    Deque constructor

--- Intervals/src/ch/ethz/intervals/DynamicWorkStealingDeque.java
+++ Intervals/src/ch/ethz/intervals/DynamicWorkStealingDeque.java
@@ -25,6 +25,19 @@ public class DynamicWorkStealingDeque implements WorkStealingQueue {
 	private AtomicStampedReference<Index> top;
 	private final Worker owner;
 
+	public DynamicWorkStealingDeque(Worker owner) {
+		this.owner = owner;
+
+		Node node1 = new Node();
+		Node node2 = new Node();
+		node1.next = node2;
+		node2.prev = node1;
+
+		bottom = new Index(node1, Node.SIZE - 1);
+		top = new AtomicStampedReference<Index>(
+				new Index(node1, Node.SIZE - 1), 0);
+	}
+
 	@Override
 	public void put(WorkItem task) {
 		// TODO Auto-generated method stub
