5d2408409347af4d028817dba8099e6ef6cc215b
Thomas Weibel <thom@beeblebrox.net>
2010-05-18 11:43:00 +0200

    Put

--- Intervals/src/ch/ethz/intervals/DynamicWorkStealingDeque.java
+++ Intervals/src/ch/ethz/intervals/DynamicWorkStealingDeque.java
@@ -40,8 +40,29 @@ public class DynamicWorkStealingDeque implements WorkStealingQueue {
 
 	@Override
 	public void put(WorkItem task) {
-		// TODO Auto-generated method stub
+		// Read bottom data
+		Node currentNode = bottom.node;
+		int currentIndex = bottom.index;
 
+		// Write data in current bottom cell
+		currentNode.tasks[currentIndex] = task;
+
+		Node newNode;
+		int newIndex;
+		if (currentIndex != 0) {
+			newNode = currentNode;
+			newIndex = currentIndex - 1;
+		} else {
+			// Allocate and link a new node
+			newNode = new Node();
+			newNode.next = currentNode;
+			currentNode.prev = newNode;
+			newIndex = Node.SIZE - 1;
+		}
+
+		// Update bottom
+		bottom.node = newNode;
+		bottom.index = newIndex;
 	}
 
 	@Override
