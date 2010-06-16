99282c13f5a70f9cc279810123424764a8928528
Thomas Weibel <thom@beeblebrox.net>
2010-06-16 15:30:30 +0200

    Place C skeleton

---
+++ Locality/jni-src/ch_ethz_hwloc_Place.c
@@ -0,0 +1,35 @@
+#include <jni.h>
+
+/* Implementation for class ch_ethz_hwloc_Place */
+
+/*
+ * Class:     ch_ethz_hwloc_Place
+ * Method:    setAffinity
+ * Signature: ([I)V
+ */
+JNIEXPORT void JNICALL Java_ch_ethz_hwloc_Place_setAffinity
+  (JNIEnv *env, jobject obj, jintArray physical_units) {
+	// TODO: setAffinity
+}
+
+/*
+ * Class:     ch_ethz_hwloc_Place
+ * Method:    getAffinity
+ * Signature: ()[I
+ */
+JNIEXPORT jintArray JNICALL Java_ch_ethz_hwloc_Place_getAffinity
+  (JNIEnv *env, jobject obj) {
+	// TODO: getAffinity;
+	return NULL;
+}
+
+/*
+ * Class:     ch_ethz_hwloc_Place
+ * Method:    getThreadId
+ * Signature: ()I
+ */
+JNIEXPORT jint JNICALL Java_ch_ethz_hwloc_Place_getThreadId
+  (JNIEnv *env, jobject obj) {
+	// TODO: getThreadId
+	return 0;
+}
