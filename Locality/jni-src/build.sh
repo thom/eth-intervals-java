f7d9f99fc57d2323f13ea559e14a063bee0a09bf
Thomas Weibel <thom@beeblebrox.net>
2010-06-17 14:20:45 +0200

    Build script is now callable from other directories too

--- Locality/jni-src/build.sh
+++ Locality/jni-src/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+cd `dirname $BASH_SOURCE`
 gcc -pthread -shared -fPIC -I/usr/lib/jvm/java-6-sun/include/ -I/usr/lib/jvm/java-6-sun/include/linux/ ch_ethz_hwloc_Place.c -o libPlace.so
 mv libPlace.so ../bin
