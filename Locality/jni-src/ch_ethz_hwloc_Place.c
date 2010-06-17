ae24f19514ab6845a79ccbbb8495a484e84d67d2
Thomas Weibel <thom@beeblebrox.net>
2010-06-17 11:07:39 +0200

    Implement getAffinity()

--- Locality/jni-src/ch_ethz_hwloc_Place.c
+++ Locality/jni-src/ch_ethz_hwloc_Place.c
@@ -1,5 +1,10 @@
+#define _GNU_SOURCE
 #include <jni.h>
 #include <syscall.h>
+#include <sched.h>
+#include <pthread.h>
+#include <stdbool.h>
+#include <unistd.h>
 #include "ch_ethz_hwloc_Place.h"
 
 /* Implementation for class ch_ethz_hwloc_Place */
@@ -20,12 +25,47 @@ Java_ch_ethz_hwloc_Place_setAffinity(JNIEnv *env, jobject obj,
 /*
  * Class:     ch_ethz_hwloc_Place
  * Method:    getAffinity
- * Signature: ()[I
+ * Signature: (I)[Z
  */
-JNIEXPORT jintArray JNICALL
+JNIEXPORT jbooleanArray JNICALL
 Java_ch_ethz_hwloc_Place_getAffinity(JNIEnv *env, jobject obj) {
-	// TODO: getAffinity;
-	return NULL;
+	int s, i;
+	int online_cpus;
+	cpu_set_t cpuset;
+	pthread_t thread;
+	jintArray result;
+
+	// Number of online processors - same as
+	// Runtime.getRuntime().availableProcessors()
+	online_cpus = sysconf(_SC_NPROCESSORS_ONLN);
+
+	result = (*env)->NewIntArray(env, online_cpus);
+	if (result == NULL) {
+		(*env)->ThrowNew(env, (*env)->FindClass(env,
+				"java/lang/OutOfMemoryError"), "Out of memory!");
+	}
+
+	thread = pthread_self();
+
+	/* Check the actual affinity mask assigned to the thread */
+	s = pthread_getaffinity_np(thread, sizeof(cpu_set_t), &cpuset);
+	if (s != 0) {
+		(*env)->ThrowNew(env, (*env)->FindClass(env,
+				"ch/ethz/hwloc/GetAffinityException"), "Couldn't get affinity!");
+	}
+
+	jboolean tmp[online_cpus];
+	for (i = 0; i < online_cpus; i++) {
+		if (CPU_ISSET(i, &cpuset)) {
+			tmp[i] = true;
+		} else {
+			tmp[i] = false;
+		}
+	}
+
+	// Move from the temporary array to the java array
+	(*env)->SetBooleanArrayRegion(env, result, 0, online_cpus, tmp);
+	return result;
 }
 
 /*
