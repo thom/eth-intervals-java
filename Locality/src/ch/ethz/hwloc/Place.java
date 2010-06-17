6e99ba3bda927cf072c6e1577392666140abb839
Thomas Weibel <thom@beeblebrox.net>
2010-06-17 11:29:47 +0200

    Implement toString

--- Locality/src/ch/ethz/hwloc/Place.java
+++ Locality/src/ch/ethz/hwloc/Place.java
@@ -9,30 +9,50 @@ public abstract class Place {
 	private int place = -1;
 
 	public Place() {
-		// TODO: constructor
+		place = -1;
 	}
 
 	public Place(int place) {
-		// TODO: constructor
+		set(place);
 	}
 
 	public void set(int place) {
 		// TODO: set
+		this.place = place;
 	}
 
 	public int get() {
-		// TODO: get
-		return 0;
+		return place;
 	}
 
 	public String toString() {
-		// TODO: toString
-		return null;
+		String result = String.format("Thread ID: %d", getThreadId());
+		result += String.format("\nPlace: %d", place);
+		for (int core : placeToCores(place)) {
+			result += String.format("\n  * Core %d", core);
+		}
+
+		try {
+			boolean[] affinities = getAffinity();
+			result += "\nLocality:";
+			for (int i = 0; i < affinities.length; i++) {
+				result += String.format("\n  * Core %d (Place %d)", i,
+						coreToPlace(i));
+			}
+		} catch (GetAffinityException e) {
+			e.printStackTrace();
+		}
+
+		return result;
 	}
+
+	public abstract int coreToPlace(int core);
+
+	public abstract int[] placeToCores(int place);
 
 	private native void setAffinity(int[] physicalUnits);
 
-	private native int[] getAffinity();
+	private native boolean[] getAffinity() throws GetAffinityException;
 
 	private native int getThreadId();
 
