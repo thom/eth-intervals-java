ea4dc48a4014351470e70b4b7c339b75935ed9bf
Thomas Weibel <thom@beeblebrox.net>
2010-06-19 16:24:10 +0200

    CacheStressTest

---
+++ Locality/src/ch/ethz/cachestress/CacheStressTest.java
@@ -0,0 +1,104 @@
+package ch.ethz.cachestress;
+
+import java.util.Arrays;
+import java.util.Random;
+
+import ch.ethz.hwloc.MafushiPlace;
+import ch.ethz.hwloc.Place;
+import ch.ethz.util.StopWatch;
+
+class CacheStressWorker extends Thread {
+	private int id;
+	private int sharedArray[];
+	private Place place;
+	private int begin, end;
+
+	public CacheStressWorker(int id, int[] sharedArray, int begin, int end) {
+		super("cache-stress-worker-" + id);
+		this.id = id;
+		this.sharedArray = sharedArray;
+		this.begin = begin;
+		this.end = end;
+		place = new MafushiPlace();
+	}
+
+	public void run() {
+		// Sum up array slice
+		int sum = 0;
+		for (int i = begin; i < end; i++) {
+			sum += sharedArray[i];
+		}
+		// Multiply array slice
+		int mult = 1;
+		for (int i = begin; i < end; i++) {
+			mult *= sharedArray[i];
+		}
+		// Sort array slice
+		Arrays.sort(sharedArray, begin, end);
+	}
+
+	public int getWorkerId() {
+		return id;
+	}
+
+	public Place getPlace() {
+		return place;
+	}
+}
+
+public abstract class CacheStressTest {
+	public final int ARRAY_SIZE = 4194296;
+
+	public abstract CacheStressWorker createCacheStressWorker(int id,
+			int[] array, int begin, int end);
+
+	public void run() {
+		StopWatch stopWatch = new StopWatch();
+
+		// Start stop watch
+		stopWatch.start();
+
+		CacheStressWorker[] worker = new CacheStressWorker[8];
+		int[] array = createRandomIntegerArray(ARRAY_SIZE);
+		int sliceSize = 4194296 / 8;
+
+		// Create worker
+		for (int i = 0; i < 8; i++) {
+			int begin = i * sliceSize;
+			int end = (i + 1) * sliceSize;
+			System.out.printf("Worker %d for array slice [%d, %d)\n", i, begin,
+					end);
+			worker[i] = createCacheStressWorker(i, array, begin, end);
+		}
+
+		// Start worker
+		for (int i = 0; i < 8; i++) {
+			worker[i].start();
+		}
+
+		// Wait for worker to finish
+		for (int i = 0; i < 8; i++) {
+			try {
+				worker[i].join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+		// Stop stop watch
+		stopWatch.stop();
+		System.out.println("\nElapsed time: " + stopWatch.getElapsedTime()
+				+ "ms");
+	}
+
+	private int[] createRandomIntegerArray(int size) {
+		Random random = new Random();
+		int[] tmp = new int[size];
+
+		for (int i = 0; i < tmp.length; i++) {
+			tmp[i] = random.nextInt();
+		}
+
+		return tmp;
+	}
+}
