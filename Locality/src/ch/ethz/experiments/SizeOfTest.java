cfb57b1685f1e24519460eeced569d7cf3c561e8
Thomas Weibel <thom@beeblebrox.net>
2010-06-19 10:11:46 +0200

    Array of size 16MB

--- Locality/src/ch/ethz/experiments/SizeOfTest.java
+++ Locality/src/ch/ethz/experiments/SizeOfTest.java
@@ -4,7 +4,7 @@ import ch.ethz.util.SizeOf;
 
 public class SizeOfTest {
 	public static void main(String[] args) {
-		int i = 1800000;
+		int i = 4194296;
 		int array[] = new int[i];
 		for (int j = 0; j < array.length; j++) {
 			array[j] = j;
@@ -13,5 +13,6 @@ public class SizeOfTest {
 		System.out.printf("int: %s\n", SizeOf.getHumanReadable(i));
 		System.out.printf("int[%d]: %s\n", i, SizeOf
 				.getHumanReadable(new int[i]));
+		System.out.printf("int[%d]: %s\n", i, SizeOf.get(new int[i]));
 	}
 }
