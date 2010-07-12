8324516001642766d55ccf10b2bf40e0a0fda928
Thomas Weibel <thom@beeblebrox.net>
2010-07-12 21:10:54 +0200

    Copy library to bin/ as well

--- Intervals/jni-src/build.sh
+++ Intervals/jni-src/build.sh
@@ -2,4 +2,5 @@
 
 cd `dirname $BASH_SOURCE`
 gcc -Wall -pthread -shared -fPIC -I/usr/lib/jvm/java-6-sun/include/ -I/usr/lib/jvm/java-6-sun/include/linux/ ch_ethz_hwloc_Affinity.c -o libAffinity.so
+cp libAffinity.so ../bin
 mv libAffinity.so ../lib
