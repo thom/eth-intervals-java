4d4bd8f3c870c4dafed4c1e4f3a0f05da5f01cde
Thomas Weibel <thom@beeblebrox.net>
2010-07-12 18:27:59 +0200

    Reformatting

--- Intervals/src/ch/ethz/intervals/InlineIntervalImpl.java
+++ Intervals/src/ch/ethz/intervals/InlineIntervalImpl.java
@@ -2,21 +2,16 @@ package ch.ethz.intervals;
 
 import java.util.Set;
 
-
-
-
 final class InlineIntervalImpl<R> extends Interval {
-
 	InlineTask<R> task;
 	private R result;
 	private Set<Throwable> errors;
 
-	InlineIntervalImpl(String name, Current current, InlineTask<R> task) 
-	{
+	InlineIntervalImpl(String name, Current current, InlineTask<R> task) {
 		super(name, current, current.inter, Point.FLAG_SYNCHRONOUS, 1, 2);
 		this.task = task;
 	}
-	
+
 	@Override
 	protected Set<Throwable> catchErrors(Set<Throwable> errors) {
 		this.errors = errors;
@@ -32,11 +27,10 @@ final class InlineIntervalImpl<R> extends Interval {
 	protected void run() {
 		result = task.run(this);
 	}
-	
+
 	R readResultOrRethrowErrors() {
-		if(errors != null)
+		if (errors != null)
 			throw new RethrownException(errors);
 		return result;
 	}
-
 }
--- Intervals/src/ch/ethz/intervals/InlineTask.java
+++ Intervals/src/ch/ethz/intervals/InlineTask.java
@@ -5,11 +5,16 @@ import ch.ethz.intervals.quals.Requires;
 
 @Subinterval
 public abstract class InlineTask<R> {
-	/** Invoked before the inline interval actually begins. 
-	 *  Can be used to add locks or <i>happens before</i> relationships. */
-	public void init(@Is("Subinterval") Interval subinterval) {}
-	
-	/** The method to be executed by the inline subinterval. */
+	/**
+	 * Invoked before the inline interval actually begins. Can be used to add
+	 * locks or <i>happens before</i> relationships.
+	 */
+	public void init(@Is("Subinterval") Interval subinterval) {
+	}
+
+	/**
+	 * The method to be executed by the inline subinterval.
+	 */
 	@Requires("method suspends Subinterval")
 	public abstract R run(@Is("Subinterval") Interval subinterval);
 }
