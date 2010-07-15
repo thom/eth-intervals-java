3d07f3ba679af87d6055a729ad0bf040ebb93c90
Thomas Weibel <thom@beeblebrox.net>
2010-07-15 14:05:25 +0200

    Update commandline options description

--- Intervals/locality/ch/ethz/util/CommandLineValues.java
+++ Intervals/locality/ch/ethz/util/CommandLineValues.java
@@ -6,19 +6,19 @@ import org.kohsuke.args4j.Option;
  * Specifies the command line values
  */
 public abstract class CommandLineValues {
-	@Option(name = "-m", aliases = { "--machine" }, usage = "machine name (default: Mafushi)")
+	@Option(name = "-m", aliases = { "--machine" }, usage = "machine name, options: Mafushi (default), Marvin")
 	private Machine machine = Machine.Mafushi;
 
-	@Option(name = "-t", aliases = { "--type" }, usage = "benchmark type (default: threads)", metaVar = "TYPE")
+	@Option(name = "-t", aliases = { "--type" }, usage = "benchmark type, options: threads (default), intervals, single", metaVar = "TYPE")
 	private BenchmarkType type = BenchmarkType.threads;
 
-	@Option(name = "-l", aliases = { "--locality" }, usage = "locality (default: Best)", metaVar = "LOCALITY")
+	@Option(name = "-l", aliases = { "--locality" }, usage = "locality, options: Best (default), Ignorant, Random, Worst, (BestCol, BestDiag, BestRow, RandomCore, RandomNode)", metaVar = "LOCALITY")
 	private Locality locality = Locality.Best;
 
-	@Option(name = "-r", aliases = { "--runs" }, usage = "number of runs (default: 10)")
+	@Option(name = "-r", aliases = { "--runs" }, usage = "number of runs, default value: 10")
 	private int runs = 10;
 
-	@Option(name = "-k", aliases = { "--kbest" }, usage = "k-best, must be <= runs (default: 3)")
+	@Option(name = "-k", aliases = { "--kbest" }, usage = "k-best, must be <= runs, default value: 3")
 	private int kbest = 3;
 
 	public Machine getMachine() {
