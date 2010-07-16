4f24751b19fe34d4ea8f29ecf01e41b3cc8936de
Thomas Weibel <thom@beeblebrox.net>
2010-07-16 14:23:51 +0200

    Best locality matrix multiplication benchmark

---
+++ Intervals/locality/ch/ethz/matmult/threads/BestLocalityBenchmark.java
@@ -0,0 +1,99 @@
+package ch.ethz.matmult.threads;
+
+import ch.ethz.hwloc.Affinity;
+import ch.ethz.hwloc.SetAffinityException;
+import ch.ethz.matmult.Main;
+import ch.ethz.matmult.Matrix;
+import ch.ethz.matmult.Quadrant;
+
+class BestLocalityMultiplicationWorker extends MultiplicationWorker {
+	public BestLocalityMultiplicationWorker(Matrix a, Matrix b, Matrix c,
+			Quadrant quadrant) {
+		super(a, b, c, quadrant);
+	}
+
+	public void run() {
+		try {
+			// TODO: keep only one best case locality
+			// TODO: fix assignment of quadrants
+			switch (quadrant) {
+			case Quadrant0:
+			case Quadrant2:
+				Affinity.set(Main.units.getNode(0 % Main.units.nodesSize()));
+				break;
+			case Quadrant1:
+			case Quadrant3:
+				Affinity.set(Main.units.getNode(1 % Main.units.nodesSize()));
+				break;
+			}
+		} catch (SetAffinityException e) {
+			e.printStackTrace();
+		}
+
+		super.run();
+	}
+
+	@Override
+	protected MultiplicationWorker createMultiplicationWorker(Matrix a,
+			Matrix b, Matrix c, Quadrant quadrant) {
+		return new BestColLocalityMultiplicationWorker(a, b, c, quadrant);
+	}
+
+	@Override
+	protected AdditionWorker createAdditionWorker(Matrix a, Matrix b, Matrix c,
+			Quadrant quadrant) {
+		return new BestColLocalityAdditionWorker(a, b, c, quadrant);
+	}
+}
+
+class BestLocalityAdditionWorker extends AdditionWorker {
+	public BestLocalityAdditionWorker(Matrix a, Matrix b, Matrix c,
+			Quadrant quadrant) {
+		super(a, b, c, quadrant);
+	}
+
+	public void run() {
+		try {
+			// TODO: keep only one best case locality
+			// TODO: fix assignment of quadrants
+			switch (quadrant) {
+			case Quadrant0:
+			case Quadrant2:
+				Affinity.set(Main.units.getNode(0 % Main.units.nodesSize()));
+				break;
+			case Quadrant1:
+			case Quadrant3:
+				Affinity.set(Main.units.getNode(1 % Main.units.nodesSize()));
+				break;
+			}
+		} catch (SetAffinityException e) {
+			e.printStackTrace();
+		}
+
+		super.run();
+	}
+
+	@Override
+	protected MultiplicationWorker createMultiplicationWorker(Matrix a,
+			Matrix b, Matrix c, Quadrant quadrant) {
+		return new BestColLocalityMultiplicationWorker(a, b, c, quadrant);
+	}
+
+	@Override
+	protected AdditionWorker createAdditionWorker(Matrix a, Matrix b, Matrix c,
+			Quadrant quadrant) {
+		return new BestColLocalityAdditionWorker(a, b, c, quadrant);
+	}
+}
+
+public class BestLocalityBenchmark extends Benchmark {
+	public BestLocalityBenchmark() {
+		super();
+	}
+
+	@Override
+	protected MultiplicationWorker createMultiplicationWorker(Matrix a,
+			Matrix b, Matrix c, Quadrant quadrant) {
+		return new BestColLocalityMultiplicationWorker(a, b, c, quadrant);
+	}
+}
