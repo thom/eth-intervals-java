6f1de016f368543ecd8964e58264f85030e5959b
Thomas Weibel <thom@beeblebrox.net>
2010-07-16 14:05:43 +0200

    Refactoring

--- Intervals/locality/ch/ethz/matmult/threads/Benchmark.java
+++ Intervals/locality/ch/ethz/matmult/threads/Benchmark.java
@@ -4,25 +4,18 @@ import ch.ethz.matmult.Main;
 import ch.ethz.matmult.Matrix;
 import ch.ethz.matmult.Quadrant;
 import ch.ethz.util.LocalityBenchmark;
-import ch.ethz.util.StopWatch;
 
 public abstract class Benchmark extends LocalityBenchmark {
 	protected abstract MultiplicationWorker createMultiplicationWorker(
 			Matrix a, Matrix b, Matrix c, Quadrant quadrant);
 
 	public long run() {
-		StopWatch stopWatch = new StopWatch();
+		startBenchmark();
 
 		// Create matrices
 		Matrix a = Matrix.random(Main.matrixDimension, Main.upperBound);
 		Matrix b = Matrix.random(Main.matrixDimension, Main.upperBound);
 		Matrix c = new Matrix(Main.matrixDimension);
-
-		// Clean JVM
-		cleanJvm();
-
-		// Start stop watch
-		stopWatch.start();
 
 		// Create worker
 		MultiplicationWorker multiplicationWorker = createMultiplicationWorker(
@@ -38,8 +31,7 @@ public abstract class Benchmark extends LocalityBenchmark {
 			e.printStackTrace();
 		}
 
-		// Stop stop watch
-		stopWatch.stop();
+		long result = stopBenchmark();
 
 		// Check result
 		if (!a.multiply(b).isEqual(c)) {
@@ -47,9 +39,6 @@ public abstract class Benchmark extends LocalityBenchmark {
 			System.exit(1);
 		}
 
-		// Clean JVM
-		cleanJvm();
-
-		return stopWatch.getElapsedTime();
+		return result;
 	}
 }
