4176908f50a08735c97dc7dee591a6f939ab1e10
Thomas Weibel <thom@beeblebrox.net>
2010-08-11 21:07:35 +0200

    Fix stats for steal()

--- Intervals/src/ch/ethz/intervals/IdempotentWorkStealingDeque.java
+++ Intervals/src/ch/ethz/intervals/IdempotentWorkStealingDeque.java
@@ -28,7 +28,7 @@ public class IdempotentWorkStealingDeque implements WorkStealingQueue {
 	public void put(WorkItem task) {
 		// Order write in (1) before write in (2)
 		int head, size, tag;
-		
+
 		while (true) {
 			ArrayData arrayData = anchor.getReference();
 			head = arrayData.head;
@@ -40,13 +40,13 @@ public class IdempotentWorkStealingDeque implements WorkStealingQueue {
 				break;
 			}
 		}
-		
+
 		// (1)
 		tasks[(head + size) % tasks.length] = task;
-		
+
 		// (2)
 		anchor.set(new ArrayData(head, size + 1), tag + 1);
-		
+
 		if (WorkerStatistics.ENABLED) {
 			owner.stats.doPut();
 		}
@@ -59,31 +59,44 @@ public class IdempotentWorkStealingDeque implements WorkStealingQueue {
 		ArrayData arrayData;
 		int head, size, tag;
 		WorkItem task;
-		
+
 		while (true) {
+			if (WorkerStatistics.ENABLED)
+				owner.stats.doStealAttempt();
+
 			// (1)
 			arrayData = anchor.getReference();
 			head = arrayData.head;
 			size = arrayData.size;
 			tag = anchor.getStamp();
 			if (size == 0) {
+				if (WorkerStatistics.ENABLED)
+					owner.stats.doStealFailure();
 				return null;
 			}
-			
+
 			// (2)
 			WorkItem[] tempTasks = tasks;
-			
+
 			// (3)
 			task = tempTasks[head % tempTasks.length];
 			int newHead = head + 1 % Integer.MAX_VALUE;
-			
+
 			// (4)
 			if (anchor.compareAndSet(arrayData,
 					new ArrayData(newHead, size - 1), tag, tag)) {
 				break;
 			}
 		}
-		
+
+		if (task == null) {
+			if (WorkerStatistics.ENABLED)
+				owner.stats.doStealFailure();
+		} else {
+			if (WorkerStatistics.ENABLED)
+				owner.stats.doStealSuccess();
+		}
+
 		return task;
 	}
 
@@ -91,51 +104,51 @@ public class IdempotentWorkStealingDeque implements WorkStealingQueue {
 	public WorkItem take() {
 		if (WorkerStatistics.ENABLED)
 			owner.stats.doTakeAttempt();
-		
+
 		ArrayData arrayData = anchor.getReference();
 		int head = arrayData.head;
 		int size = arrayData.size;
 		int tag = anchor.getStamp();
-		
+
 		if (size == 0) {
 			if (WorkerStatistics.ENABLED)
 				owner.stats.doTakeFailure();
 			return null;
 		}
-		
+
 		WorkItem task = tasks[(head + size - 1) % tasks.length];
 		anchor.set(new ArrayData(head, size - 1), tag);
-		
+
 		if (WorkerStatistics.ENABLED) {
 			if (task == null)
 				owner.stats.doTakeFailure();
 			else
 				owner.stats.doTakeSuccess();
 		}
-		
+
 		return task;
 	}
 
 	private void expand() {
 		// Order writes in (1) and (2) before write in (3)
 		// Order write in (3) before write in put:(2)
-		
+
 		if (WorkerStatistics.ENABLED)
 			owner.stats.doGrow();
-		
+
 		ArrayData arrayData = anchor.getReference();
 		int head = arrayData.head;
 		int size = arrayData.size;
-		
+
 		// (1)
 		WorkItem[] tempTasks = new WorkItem[2 * size];
-		
+
 		for (int i = 0; i < size; i++) {
 			// (2)
 			tempTasks[(head + i) % tempTasks.length] = tasks[(head + i)
 					% tasks.length];
 		}
-		
+
 		// (3)
 		tasks = tempTasks;
 	}
